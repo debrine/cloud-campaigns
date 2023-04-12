e6d56acd7f3ec885612783b8bf39dd7452eeb008
Dylan Brine <dylanbrine@gmail.com>
2023-04-11 21:19:27 -0300

    added some new character sheet models

---
+++ src/models/character-sheet-models/character-summary.model.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const CharacterSummaryModel = z.object({
+  id: z.string(), // UUID Type?
+  characterName: z.string(),
+  playerName: z.string(),
+  characterClass: z.string(), // Enum?
+  characterLevel: z.number().optional(),
+  characterRace: z.string(), // Enum?
+});
